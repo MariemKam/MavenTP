e5791276e5b0911e862766ff76c12a80e9121e8d
MariemKam <70704270+MariemKam@users.noreply.github.com>
2020-09-18 10:07:48 +0100

    Update App.java
    ajout de ligne sysoutprint: test modif git - trigger build jenkins

--- maven-demo/src/main/java/com/codebind/App.java
+++ maven-demo/src/main/java/com/codebind/App.java
@@ -2,6 +2,7 @@ package com.codebind;
 
 public class App {
     public String Sample() {
+        System.out.println("Test: modif dans git pr voir trigger build en jenkins");
     	return "sample code";
     }
 }
